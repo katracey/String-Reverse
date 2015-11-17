41aa35094bc686792968c2cee6cf5bac6ac20573
Katie <katracey@gmail.com>
2015-11-17 14:40:04 -0500

    1

---
+++ 150920_StringReverse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _150920_StringReverse
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Provide a string to reverse.");
+            string text = Console.ReadLine();
+            
+            for (int i = (text.Length - 1); i >= 0; i--)
+            {
+                Console.Write(text[i]);
+            }
+
+
+        }
+    }
+}
